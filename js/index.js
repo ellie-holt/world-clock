ce3c27bc6e8539029d18192bd2ef71a9fd66c596
Ellie <eleanorholt97@gmail.com>
2024-04-07 22:51:41 +0100

    Add ability to select current location, some refactoring involving DOM manipulation outside of main functions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 function onTimezoneChange(event) {
   if (event.target.value.length > 0) {
     currentCityTimezone = event.target.value;
+    if (currentCityTimezone === "current") {
+      currentCityTimezone = moment.tz.guess();
+    }
     updateMainCityCard(currentCityTimezone);
   }
 }
@@ -12,17 +15,16 @@ function updateMainCityCard(timezone) {
 }
 
 function updateAdditionalCityCards() {
-  let cards = document.querySelectorAll(".additional");
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < additionalCityCards.length; i++) {
     if (additionalTimezones[i]) {
       let timezone = additionalTimezones[i];
-      cards[i].querySelector(".city-title").innerHTML = timezone
+      additionalCityCards[i].querySelector(".city-title").innerHTML = timezone
         .replace("_", " ")
         .split("/")[1];
-      cards[i].querySelector(".city-time").innerHTML = moment
+      additionalCityCards[i].querySelector(".city-time").innerHTML = moment
         .tz(timezone)
         .format("HH:mm");
-      cards[i].querySelector(".city-date").innerHTML = moment
+      additionalCityCards[i].querySelector(".city-date").innerHTML = moment
         .tz(timezone)
         .format("dddd Do MMMM YYYY");
     }
@@ -31,6 +33,13 @@ function updateAdditionalCityCards() {
 
 function updateTime() {
   timeElement.innerHTML = moment.tz(currentCityTimezone).format("HH:mm");
+  additionalCityCards.forEach((card, index) => {
+    if (additionalTimezones[index]) {
+      card.querySelector(".city-time").innerHTML = moment
+        .tz(additionalTimezones[index])
+        .format("HH:mm");
+    }
+  });
 }
 
 let currentCityTimezone = "Europe/London"; //Default current timezone
@@ -47,6 +56,8 @@ let mainCityCard = document.querySelector(".city-card.main");
 let titleElement = mainCityCard.querySelector(".city-title");
 let timeElement = mainCityCard.querySelector(".city-time");
 let dateElement = mainCityCard.querySelector(".city-date");
+
+let additionalCityCards = document.querySelectorAll(".additional");
 
 let citySelectElement = document.querySelector("#city-select");
 citySelectElement.addEventListener("change", onTimezoneChange);
